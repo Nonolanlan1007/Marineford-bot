f9f090d9c37e60d49140c09c75940a98703d974e
Nonolanlan1007 <nolhan.dev@gmail.com>
2021-10-25 09:42:54 +0200

    a

--- models/botconfig.js
+++
@@ -1,7 +0,0 @@
-const { Schema, model } = require("mongoose");
-
-botShema = new Schema({
-    suggests: { type: Number, required: false }
-})
-
-robotShema = module.exports = model("botconfig", botShema)
